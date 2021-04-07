cf833717c6785c5f6baddf54b122696b2b067c8e
jlblanco-lb <jlblanco.lb@gmail.com>
2021-04-07 13:47:24 +0200

    API platform, doctrine and migrations bundles added.

---
+++ backend/src/Entity/Phone.php
@@ -0,0 +1,162 @@
+<?php
+
+namespace App\Entity;
+
+use ApiPlatform\Core\Annotation\ApiResource;
+use App\Repository\PhoneRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ApiResource()
+ * @ORM\Entity(repositoryClass=PhoneRepository::class)
+ */
+class Phone
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $description;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $color;
+
+    /**
+     * @ORM\Column(type="float")
+     */
+    private $price;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $imageFileName;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $screen;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $processor;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $ram;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getColor(): ?string
+    {
+        return $this->color;
+    }
+
+    public function setColor(string $color): self
+    {
+        $this->color = $color;
+
+        return $this;
+    }
+
+    public function getPrice(): ?float
+    {
+        return $this->price;
+    }
+
+    public function setPrice(float $price): self
+    {
+        $this->price = $price;
+
+        return $this;
+    }
+
+    public function getImageFileName(): ?string
+    {
+        return $this->imageFileName;
+    }
+
+    public function setImageFileName(string $imageFileName): self
+    {
+        $this->imageFileName = $imageFileName;
+
+        return $this;
+    }
+
+    public function getScreen(): ?string
+    {
+        return $this->screen;
+    }
+
+    public function setScreen(string $screen): self
+    {
+        $this->screen = $screen;
+
+        return $this;
+    }
+
+    public function getProcessor(): ?string
+    {
+        return $this->processor;
+    }
+
+    public function setProcessor(string $processor): self
+    {
+        $this->processor = $processor;
+
+        return $this;
+    }
+
+    public function getRam(): ?int
+    {
+        return $this->ram;
+    }
+
+    public function setRam(int $ram): self
+    {
+        $this->ram = $ram;
+
+        return $this;
+    }
+}
